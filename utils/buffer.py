0e5c0016196d35094c022f42e5e650bd4b9c86dc
Martin Menabue <martinmnb@live.it>
2023-05-14 22:42:08 +0200

    update

--- utils/buffer.py
+++ utils/buffer.py
@@ -9,6 +9,7 @@ from typing import Tuple
 import numpy as np
 import torch
 import torch.nn as nn
+from PIL import Image
 
 
 def icarl_replay(self, dataset, val_set_split=0):
@@ -122,9 +123,12 @@ class Buffer:
         for attr_str in self.attributes:
             attr = eval(attr_str)
             if attr is not None and not hasattr(self, attr_str):
-                typ = torch.int64 if attr_str.endswith('els') else torch.float32
-                setattr(self, attr_str, torch.zeros((self.buffer_size,
-                        *attr.shape[1:]), dtype=typ, device=self.device))
+                if attr_str == 'examples' and type(attr[0]) == Image.Image:
+                    setattr(self, attr_str, np.array([None for _ in range(self.buffer_size)], dtype=object))
+                else:
+                    typ = torch.int64 if attr_str.endswith('els') else torch.float32
+                    setattr(self, attr_str, torch.zeros((self.buffer_size,
+                            *attr.shape[1:]), dtype=typ, device=self.device))
 
     def add_data(self, examples, labels=None, logits=None, task_labels=None):
         """
@@ -138,11 +142,14 @@ class Buffer:
         if not hasattr(self, 'examples'):
             self.init_tensors(examples, labels, logits, task_labels)
 
-        for i in range(examples.shape[0]):
+        for i in range(len(examples)):
             index = reservoir(self.num_seen_examples, self.buffer_size)
             self.num_seen_examples += 1
             if index >= 0:
-                self.examples[index] = examples[i].to(self.device)
+                if type(examples[i]) == Image.Image:
+                    self.examples[index] = examples[i]
+                else:
+                    self.examples[index] = examples[i].to(self.device)
                 if labels is not None:
                     self.labels[index] = labels[i].to(self.device)
                 if logits is not None:
@@ -164,7 +171,11 @@ class Buffer:
                                   size=size, replace=False)
         if transform is None:
             def transform(x): return x
-        ret_tuple = (torch.stack([transform(ee.cpu()) for ee in self.examples[choice]]).to(self.device),)
+        if type(self.examples[0]) == Image.Image:
+            ret_tuple = (torch.stack([transform(ee)
+                                      for ee in self.examples[choice]]).to(self.device),)
+        else:
+            ret_tuple = (torch.stack([transform(ee.cpu()) for ee in self.examples[choice]]).to(self.device),)
         for attr_str in self.attributes[1:]:
             if hasattr(self, attr_str):
                 attr = getattr(self, attr_str)
