1056a41627e074cbdabdc3f50c0f386ff8974aab
loribonna <loribonna@gmail.com>
2023-12-06 16:29:23 +0100

    Initial docs

---
+++ docs/conf.py
@@ -0,0 +1,27 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Mammoth'
+copyright = '2023, Pietro Buzzega, Matteo Boschini, Angelo Porrello, Lorenzo Bonicelli, Aniello Panariello'
+author = 'Pietro Buzzega, Matteo Boschini, Angelo Porrello, Lorenzo Bonicelli, Aniello Panariello'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,21 @@
+.. Mammoth documentation master file, created by
+   sphinx-quickstart on Tue Dec  5 23:41:27 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Mammoth's documentation!
+**BETA**
+===================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
