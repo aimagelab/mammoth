e92abd16ce14ec353a16fa529885cf77e655d5b8
loribonna <loribonna@gmail.com>
2024-10-08 21:47:50 +0200

    Update l2p and dualprompt reproducibility

--- NOTICE.md
+++ NOTICE.md
@@ -1,3 +1,5 @@
 This project is licensed under the MIT License. However, some files in this repository are under the Apache 2.0 License as noted below:
 
 - backbone/vit.py (Apache 2.0 License)
+- models/l2p.py (Apache 2.0 License)
+- models/dualprompt.py (Apache 2.0 License)
--- models/l2p_utils/l2p_model.py
+++ models/l2p_utils/l2p_model.py
@@ -1,3 +1,5 @@
+import os
+import numpy as np
 import torch
 import torch.nn as nn
 from datasets import get_dataset
@@ -34,6 +36,83 @@ class L2PModel(nn.Module):
             use_prompt_mask=args.use_prompt_mask,
         )
 
+        if args.use_original_ckpt:
+            # download ckpt from https://storage.googleapis.com/vit_models/imagenet21k/ViT-B_16.npz
+
+            if not os.path.exists('./data/imagenet21k_ViT-B_16.npz'):
+                os.system('wget https://storage.googleapis.com/vit_models/imagenet21k/ViT-B_16.npz -P ./data/')
+
+            lf = np.load('data/imagenet21k_ViT-B_16.npz')
+            ckpt = {k: lf[k] for k in lf.files}
+
+            def translate_name(name):
+                name = name.replace('Transformer/', '')
+                name = name.replace('encoderblock_', 'blocks.')
+                name = name.replace('/', '.')
+                name = name.replace('LayerNorm_', 'norm')
+                name = name.replace('norm0', 'norm1')
+                name = name.replace('MlpBlock_3', 'mlp')
+                name = name.replace('Dense_0', 'fc1')
+                name = name.replace('Dense_1', 'fc2')
+                name = name.replace('MultiHeadDotProductAttention_1', 'attn')
+                name = name.replace('kernel', 'weight')
+                name = name.replace('out', 'proj')
+                name = name.replace('scale', 'weight')
+                name = name.replace('cls', 'cls_token')
+                name = name.replace('embedding', 'patch_embed.proj')
+                name = name.replace('posembed_input.pos_patch_embed.proj', 'pos_embed')
+                name = name.replace('encoder_norm', 'norm')
+                return name
+            ckpt = {translate_name(k): v for k, v in ckpt.items()}
+
+            for block_id in range(12):
+                # convert qkv
+                q = ckpt[f'blocks.{block_id}.attn.query.weight'].reshape(768, -1)
+                k = ckpt[f'blocks.{block_id}.attn.key.weight'].reshape(768, -1)
+                v = ckpt[f'blocks.{block_id}.attn.value.weight'].reshape(768, -1)
+                qkv = np.concatenate([q, k, v], axis=1)
+                ckpt[f'blocks.{block_id}.attn.qkv.weight'] = qkv
+                ckpt.pop(f'blocks.{block_id}.attn.query.weight')
+                ckpt.pop(f'blocks.{block_id}.attn.key.weight')
+                ckpt.pop(f'blocks.{block_id}.attn.value.weight')
+
+                q = ckpt[f'blocks.{block_id}.attn.query.bias'].reshape(-1)
+                k = ckpt[f'blocks.{block_id}.attn.key.bias'].reshape(-1)
+                v = ckpt[f'blocks.{block_id}.attn.value.bias'].reshape(-1)
+                qkv = np.concatenate([q, k, v], axis=0)
+                ckpt[f'blocks.{block_id}.attn.qkv.bias'] = qkv
+                ckpt.pop(f'blocks.{block_id}.attn.query.bias')
+                ckpt.pop(f'blocks.{block_id}.attn.key.bias')
+                ckpt.pop(f'blocks.{block_id}.attn.value.bias')
+
+                # permute
+                ckpt[f'blocks.{block_id}.mlp.fc1.weight'] = ckpt[f'blocks.{block_id}.mlp.fc1.weight'].T
+                ckpt[f'blocks.{block_id}.mlp.fc2.weight'] = ckpt[f'blocks.{block_id}.mlp.fc2.weight'].T
+
+                ckpt[f'blocks.{block_id}.attn.qkv.weight'] = ckpt[f'blocks.{block_id}.attn.qkv.weight'].T
+                ckpt[f'blocks.{block_id}.attn.proj.weight'] = ckpt[f'blocks.{block_id}.attn.proj.weight'].reshape(-1, 768).T
+            ckpt['patch_embed.proj.weight'] = ckpt['patch_embed.proj.weight'].transpose(-1, -2, -4, -3)
+
+            # remove head
+            del ckpt['head.weight']
+            del ckpt['head.bias']
+            del ckpt['pre_logits.weight']
+            del ckpt['pre_logits.bias']
+
+            # convert to torch
+            ckpt = {k: torch.from_numpy(v) for k, v in ckpt.items()}
+
+            unexpected, missing = self.original_model.load_state_dict(ckpt, strict=False)
+            assert len([x for x in missing if 'head' not in x]) == 0, f"Missing keys: {missing}"
+            assert len([x for x in unexpected if 'head' not in x]) == 0, f"Unexpected keys: {unexpected}"
+
+            # extend pos_embed for the prompts
+            ckpt['pos_embed'] = torch.cat((ckpt['pos_embed'], self.model.pos_embed[:, ckpt['pos_embed'].shape[1]:]), dim=1)
+
+            unexpected, missing = self.model.load_state_dict(ckpt, strict=False)
+            assert len([x for x in missing if 'prompt' not in x and 'head' not in x]) == 0, f"Missing keys: {missing}"
+            assert len([x for x in unexpected if 'prompt' not in x and 'head' not in x]) == 0, f"Unexpected keys: {unexpected}"
+
         if args.freeze:
             # all parameters are frozen for original vit model
             for p in self.original_model.parameters():
@@ -44,7 +123,7 @@ class L2PModel(nn.Module):
                 if n.startswith(tuple(args.freeze)):
                     p.requires_grad = False
 
-    def forward(self, x, return_outputs=False):
+    def forward(self, x, return_reduce_sim_loss=False):
         with torch.no_grad():
             if self.original_model is not None:
                 original_model_output = self.original_model(x)
@@ -55,7 +134,8 @@ class L2PModel(nn.Module):
         outputs = self.model(x, task_id=-1, cls_features=cls_features, train=self.training)
 
         logits = outputs['logits']
-        if return_outputs:
-            return outputs
+        reduce_sim = outputs['reduce_sim'] if 'reduce_sim' in outputs else None
+        if return_reduce_sim_loss:
+            return {'logits': logits, 'reduce_sim': reduce_sim}
         else:
             return logits
